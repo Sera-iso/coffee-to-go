29b77668e281a30dcd4c2d55a78f517502ca7946
Sera_Is <serena.isone@adidas.com>
2022-09-17 22:09:22 +0200

    Add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+export default function Footer() {
+    return (
+        <div className='Footer'>
+            <span><p>© Coded by Sera and hosted on GitHub. All rights reserved.</p></span>
+            <span><a href="https://github.com/Sera-iso/coffee-to-go" target="_blank" rel="noreferrer"><i className='fa-brands fa-github'></i></a></span>
+        </div>
+    )
+}
