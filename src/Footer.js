0119f5b3e286dde7c958fd213fee27b2f8e8da14
Sera_Is <serena.isone@adidas.com>
2022-09-18 17:34:13 +0200

    Style footer and add contact info

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
 import './Footer.css';
 export default function Footer() {
     return (
-        <div className='Footer'>
+        <div className='Footer' id="contacts">
             <span><p>© Coded by Sera and hosted on GitHub. All rights reserved.</p></span>
-            <span><a href="https://github.com/Sera-iso/coffee-to-go" target="_blank" rel="noreferrer"><i className='fa-brands fa-github'></i></a></span>
+            <span><a href="https://github.com/Sera-iso/coffee-to-go" target="_blank" rel="noreferrer"><i className='fa-brands fa-github'></i></a>
+            <a href="mailto:someone@yoursite.com" target="_blank" rel="noreferrer"><i className="fa-solid fa-envelope"></i></a></span>
         </div>
     )
 }
