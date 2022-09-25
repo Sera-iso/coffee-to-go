4c36d74cdcc77fd463fd3727cd7535a23b7862eb
Sera_Is <serena.isone@adidas.com>
2022-09-25 14:42:06 +0200

    Tweak content in Hero component

--- src/Hero.js
+++ src/Hero.js
@@ -15,7 +15,7 @@ export default function Hero() {
           <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl">
-                <span className="block xl:inline">No queues, no waiting</span>{' '}
+                <span className="block xl:inline">Coffee to go</span>{' '}
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
                 Don't queue to pay, or wait in line for your order to be ready. Order online while you get to the shop, pick it up and go!
@@ -26,7 +26,7 @@ export default function Hero() {
                     href="#productList"
                     className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-900 px-8 py-3 text-base font-medium text-white hover:bg-yellow-800 md:py-4 md:px-10 md:text-lg"
                   >
-                    Buy breakfast
+                    Buy a coffee
                   </a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
