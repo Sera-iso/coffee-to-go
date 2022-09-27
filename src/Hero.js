b9813a3f76f74dec144cface137f43f6e563883a
Sera_Is <serena.isone@adidas.com>
2022-09-27 11:01:31 +0200

    Refactor Hero component to make it less complex

--- src/Hero.js
+++ src/Hero.js
@@ -1,54 +1,34 @@
 import { Link } from 'react-router-dom'
 export default function Hero() {
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="mx-auto max-w-7xl">
-        <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
- <svg
-            className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
-            fill="currentColor"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="none"
-            aria-hidden="true"
-          >
-            <polygon points="50,0 100,0 50,100 0,100" />
-          </svg>
-          <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl">
-                <span className="block xl:inline">Coffee to go</span>{' '}
-              </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-                Don't queue to pay, or wait in line for your order to be ready. Order online while you get to the shop, pick it up and go!
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <a
-                    href="#productList"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-900 px-8 py-3 text-base font-medium text-white hover:bg-yellow-800 md:py-4 md:px-10 md:text-lg"
-                  >
-                    Buy a coffee
-                  </a>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <Link to="/signup"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-400 px-8 py-3 text-base font-medium text-yellow-900 hover:bg-yellow-300 md:py-4 md:px-10 md:text-lg"
-                  >
-                    Get subscription
-                  </Link>
-                </div>
-              </div>
+    <div className="w-full h-96 bg-[url('https://images.unsplash.com/photo-1509785307050-d4066910ec1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1328&q=80')] bg-cover bg-center flex justify-center items-center">
+      <main>
+        <div className="flex flex-col justify-center items-center bg-black/50 p-10">
+          <h1 className="text-center text-5xl text-white font-bold drop-shadow-lg">
+            <span>Coffee to go</span>
+          </h1>
+          <p className="mt-5 text-center text-lg text-white opacity-70">
+            Don't queue to pay, or wait in line for your order to be ready.<br/> Order online while you get to the shop, pick it up and go!
+          </p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <a
+                href="#productList"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-900 px-8 py-3 text-base font-medium text-white hover:bg-yellow-800 md:py-4 md:px-10 md:text-lg"
+              >
+                Buy a coffee
+              </a>
             </div>
-          </main>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <Link to="/signup"
+                className="flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-400 px-8 py-3 text-base font-medium text-yellow-900 hover:bg-yellow-300 md:py-4 md:px-10 md:text-lg"
+              >
+                Get subscription
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
-          src="https://images.unsplash.com/photo-1574377727482-e82003fd3609?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-          alt="Breakfast"
-        />
-      </div>
+      </main>
     </div>
   )
 }
