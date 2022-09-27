7b5038947c01fa141ccee43c600c1bcbf990144e
Sera_Is <serena.isone@adidas.com>
2022-09-27 20:50:42 +0200

    Style mobile view for Footer and Hero component

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,12 @@ export default function Footer() {
     return (
         <>
             <hr className='border border-gray-100'/>
-            <div className='flex justify-between text-slate-300' id="contacts">
-                <span><p className='my-10 font-thin'>© Coded by Sera and hosted on GitHub. All rights reserved.</p></span>
-                <span className='text-3xl my-8'><a href="https://github.com/Sera-iso/coffee-to-go" target="_blank" rel="noreferrer"><i className='fa-brands fa-github px-6'></i></a>
-                <a href="mailto:someone@yoursite.com" target="_blank" rel="noreferrer"><i className="fa-solid fa-envelope px-6"></i></a></span>
+            <div className='flex items-center justify-between text-slate-300 py-6 md:space-x-10' id="contacts">
+                <span><p className='flex justify-start font-thin'>© Coded by Sera and hosted on GitHub. All rights reserved.</p></span>
+                <span className='flex justify-end text-3xl'>
+                    <a href="https://github.com/Sera-iso/coffee-to-go" target="_blank" rel="noreferrer"><i className='fa-brands fa-github px-6'></i></a>
+                    <a href="mailto:someone@yoursite.com" target="_blank" rel="noreferrer"><i className="fa-solid fa-envelope px-6"></i></a>
+                </span>
             </div>
         </>
     )
