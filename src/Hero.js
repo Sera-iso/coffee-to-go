2af15990a9e6fda7758f0fc63f57a760c20bf7a0
Sera_Is <serena.isone@adidas.com>
2022-09-27 20:50:48 +0200

    Update Hero.js

--- src/Hero.js
+++ src/Hero.js
@@ -3,11 +3,11 @@ export default function Hero() {
   return (
     <div className="w-full h-96 bg-[url('https://images.unsplash.com/photo-1509785307050-d4066910ec1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1328&q=80')] bg-cover bg-center flex justify-center items-center">
       <main>
-        <div className="flex flex-col justify-center items-center bg-black/50 p-10">
+        <div className="flex flex-col justify-center items-center bg-black/30 p-4">
           <h1 className="text-center text-5xl text-white font-bold drop-shadow-lg">
             <span>Coffee to go</span>
           </h1>
-          <p className="mt-5 text-center text-lg text-white opacity-70">
+          <p className="mt-5 text-center text-lg text-white">
             Don't queue to pay, or wait in line for your order to be ready.<br/> Order online while you get to the shop, pick it up and go!
           </p>
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
