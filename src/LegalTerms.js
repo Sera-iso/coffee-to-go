7a206b1951518f2fa2ebd85d6b4eb366fbd7a3d9
Sera_Is <serena.isone@adidas.com>
2022-10-02 20:52:25 +0200

    Remove unused comments

--- src/LegalTerms.js
+++ src/LegalTerms.js
@@ -16,22 +16,18 @@ export default function LegalTerms() {
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-xl font-semibold">
                     Terms of Service
                   </h3>
                 </div>
-                {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-slate-500 text-base leading-relaxed">
                     Today, this site is for exercise purposes only.<br/>
                     If you choose to use this Service, then you agree to the collection and use of your information.
                   </p>
                 </div>
-                {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-yellow-900 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
