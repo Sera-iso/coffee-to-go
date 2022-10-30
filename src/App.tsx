f868082344491d5818e3872410b4de37cc10cf48
Serena Isone <isonserena@hotmail.it>
2022-10-30 20:27:04 +0100

    Add shopping cart context

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./Footer";
-import Home from "./pages/Home";
-import LogIn from "./Log-in";
-import SignUp from "./Sign-up";
-//import Cart from "./Cart";
-import NoPage from "./NoPage";
-//import ProductDetails from "./ProductDetails";
+import Footer from "./components/Footer";
+import Menu from "./pages/Menu";
+import LogIn from "./components/Log-in";
+import SignUp from "./components/Sign-up";
+import NoPage from "./pages/NotFound";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 export default function App() {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}>
-          {/* <Route path="/:name" element={<ProductDetails />} />*/}
-        </Route>
-        {/*<Route path="/cart" element={<Cart />} />*/}
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/*" element={<NoPage />} />
-      </Routes>
-      <Footer />
-    </div>
+    <ShoppingCartProvider>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </ShoppingCartProvider>
   );
 }
